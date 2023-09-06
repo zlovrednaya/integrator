6b0c2f122b6da3a4d750b718de461abec6f84bec
daria <daria.gosteva@klientiks.ru>
2023-09-06 10:34:22 +0300

    html

--- resources/js/content/TelegramBot.jsx
+++ resources/js/content/TelegramBot.jsx
@@ -19,10 +19,13 @@ export default function Home(){
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Token</label>
                 <input type="token" class="form-control" id="token" placeholder="token" />
+                <label for="exampleFormControlInput1" class="form-label">Message</label>
                 <input type="message" class="form-control" id="message" placeholder="message" />
                 
                 <button type="button" class="btn btn-light" onClick={sendRequest}>Send message</button>
             </div>
+            <div>Chat</div>
+            <div>here are messages with bot</div>
 
         </div>
     </div>);
