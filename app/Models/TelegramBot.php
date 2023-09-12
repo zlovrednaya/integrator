e59b9c29dce7b5b7b792c0fe1d1eedfcc5159c41
Daria <102523614+zlovrednaya@users.noreply.github.com>
2023-09-12 16:26:54 +0300

    Update TelegramBot.php

--- app/Models/TelegramBot.php
+++ app/Models/TelegramBot.php
@@ -3,7 +3,7 @@ hat<?php
 namespace App\Models;
 //integrator_talk
 class TelegramBot extends Integrator{
-    public $api_key = '6191341752:AAFokKPO4EX6tLZKqW9x521B0VqQ2XBbpm8';
+    public $api_key = '';
     public $url = "https://api.telegram.org/bot";
     public $path;
 
